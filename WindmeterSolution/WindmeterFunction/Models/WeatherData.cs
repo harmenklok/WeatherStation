f8813b949297db4b651432ecdc56447e1b478988
Harmen Klok <hklok@hso.com>
2024-10-21 21:30:28 +0200

    Create function and set initial values

---
+++ WindmeterSolution/WindmeterFunction/Models/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿namespace WindmeterFunction.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class WeatherData
+    {
+        public int windForceBft {  get; set; }
+        public float tempCel {  get; set; }
+        public int windDirectionDeg { get; set; }
+    }
+}
