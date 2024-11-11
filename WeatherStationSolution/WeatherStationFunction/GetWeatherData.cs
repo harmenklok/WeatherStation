e012ef9cd18e5763b5565662e29a38b4179ce9f8
Harmen Klok <hklok@hso.com>
2024-11-11 15:20:43 +0100

    Revert 0 bft testing

--- WeatherStationSolution/WeatherStationFunction/GetWeatherData.cs
+++ WeatherStationSolution/WeatherStationFunction/GetWeatherData.cs
@@ -46,7 +46,7 @@ namespace WeatherStationFunction
 
                     WeatherData weatherData = new()
                     {
-                        WindForceBft = 0, //Convert.ToInt16(windForce),
+                        WindForceBft = Convert.ToInt16(windForce),
                         TempCel = Convert.ToInt16(Math.Round(weatherResponse.main.temp, 0)),
                         WindDirComp = windCompDirs[compIdx],
                         UpdateInterval = updateInterval
